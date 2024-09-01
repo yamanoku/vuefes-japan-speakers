090c7eeec301a4e99187c06527ab2fe450939f29
yamanoku <0910yama@gmail.com>
2024-09-01 15:15:24 +0900

    add icons

--- app/components/FooterComponent.vue
+++ app/components/FooterComponent.vue
@@ -1,26 +1,38 @@
 <template>
   <footer class="w-4/5 ms-auto me-auto py-6">
     <dl>
-      <div class="flex flex-wrap gap-2.5">
-        <dt><b>Reference Code</b></dt>
+      <div class="flex flex-wrap gap-2">
+        <dt class="flex items-center gap-1">
+          <UIcon name="i-heroicons-solid-academic-cap" />
+          Reference Code
+        </dt>
         <dd>
           <a class="underline hover:no-underline text-gray-500 dark:text-gray-400" href="https://github.com/ima1zumi/RubyKaigi-speakers" target="_blank">ima1zumi/RubyKaigi-speakers</a>
         </dd>
       </div>
-      <div class="flex flex-wrap gap-2.5">
-        <dt><b>Reference Source</b></dt>
+      <div class="flex flex-wrap gap-2">
+        <dt class="flex items-center gap-1">
+          <UIcon name="i-heroicons-solid-academic-cap" />
+          Reference Source
+        </dt>
         <dd>
           <a class="underline hover:no-underline text-gray-500 dark:text-gray-400" href="https://github.com/vuejs-jp" target="_blank">Vue.js Japan User Group</a>
         </dd>
       </div>
-      <div class="flex flex-wrap gap-2.5">
-        <dt><b>Source Code</b></dt>
+      <div class="flex flex-wrap gap-2">
+        <dt class="flex items-center gap-1">
+          <UIcon name="i-heroicons-solid-tag" />
+          Source Code
+        </dt>
         <dd>
           <a class="underline hover:no-underline text-gray-500 dark:text-gray-400" href="https://github.com/yamanoku/vuefes-japan-speakers" target="_blank">yamanoku/vuefes-japan-speakers</a>
         </dd>
       </div>
-      <div class="flex flex-wrap gap-2.5">
-        <dt><b>Article</b></dt>
+      <div class="flex flex-wrap gap-2">
+        <dt class="flex items-center gap-1">
+          <UIcon name="i-heroicons-solid-book-open w-5 h-5" />
+          Article
+        </dt>
         <dd>
           <a class="underline hover:no-underline text-gray-500 dark:text-gray-400" href="https://zenn.dev/yamanoku/articles/vuefes-japan-speakers" target="_blank">Vue Fes Japanの歴代スピーカー一覧を見られるページを作った</a>
         </dd>
